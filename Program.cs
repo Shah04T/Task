3ce761eab8ea9ea1fd88cdd997c9e6c6be3fd426
Shadat <miatata48@gmail.com>
2024-05-27 00:58:38 +0600

    Первая функция

--- Program.cs
+++ Program.cs
@@ -2,23 +2,29 @@
 
 Console.Write("Сколько предложений вы будите вводить?:");
 int num = Convert.ToInt32(Console.ReadLine());
-string results = string.Empty;
-int count = 0;
-string[] sentences = new string[num];
-for (int i = 0; i < num; i++)
+string[] sentences = GetReadysentences(num);
+
+static string[] GetReadysentences(num)
 {
+    string[] input = new string(num);
+    int count = 0;
+
+    for (int i = 0; i < num; i++)
+    {
 #pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
-    string fchr = Console.ReadLine();
+        string fchr = Console.ReadLine();
 #pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
 #pragma warning disable CS8602 // Dereference of a possibly null reference.
-    if (fchr.Length <= 3)
-    {
-        sentences[count]=fchr;
-        count++;
-    }
+        if (fchr.Length <= 3)
+        {
+            input[count] = fchr;
+            count++;
+        }
 #pragma warning restore CS8602 // Dereference of a possibly null reference.
-    fchr = string.Empty;
+        fchr = string.Empty;
+    }
 }
+
 results += "[ ";
 for (int i = 0; i < count; i++)
 {
