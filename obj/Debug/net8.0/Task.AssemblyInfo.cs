4e538dca0bd5f4517be412ff9def71985990a4aa
Shadat <miatata48@gmail.com>
2024-05-27 00:39:50 +0600

    нужно делать меньше проб

--- obj/Debug/net8.0/Task.AssemblyInfo.cs
+++ obj/Debug/net8.0/Task.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc178e88dfb375a463c21723592a55caca098b0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdb05fab461d04f242c4cd12076d42d6f27f7c68")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
