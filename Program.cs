bdb05fab461d04f242c4cd12076d42d6f27f7c68
Shadat <miatata48@gmail.com>
2024-05-27 00:30:59 +0600

    well, it took me for 5 hours

--- Program.cs
+++ Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using System.Globalization;
+
+Console.Write("Сколько предложений вы будите вводить?:");
+int num = Convert.ToInt32(Console.ReadLine());
+string results = string.Empty;
+int count = 0;
+string[] sentences = new string[num];
+for (int i = 0; i < num; i++)
+{
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+    string fchr = Console.ReadLine();
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+    if (fchr.Length <= 3)
+    {
+        sentences[count]=fchr;
+        count++;
+    }
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+    fchr = string.Empty;
+}
+results += "[ ";
+for (int i = 0; i < count; i++)
+{
+    results += sentences[i];
+    if (i != count-1)
+    {
+        results += ", ";
+    }
+}
+results += "]";
+Console.Write(results);
